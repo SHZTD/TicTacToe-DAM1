c5ea296412f3a2f6a757b3c66f2fa225216b5745
SHZTD <tdrag@elpuig.xeill.net>
2024-04-16 17:47:43 +0200

    xd

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main extends Tui {
     public static void main(String[] args) {
         Tui tui = new Tui();
-        tui.tuiGraphics(5, 5);
+        tui.tuiConfig();
     }
 }
--- src/Tui.java
+++ src/Tui.java
@@ -1,12 +1,10 @@
 public class Tui {
-    public int xsize = 0, ysize = 0;
-
-    public void tuiGraphics(int xsize, int ysize) {
-        for (int i = 0; i < xsize; i++) {
-            for (int j = 0; j < ysize; j++) {
-                System.out.print("[+]");
-            }
-            System.out.print("\n");
+    protected void tuiConfig() {
+        System.out.print("╔");
+        for(int i = 0; i < 30; i++) {
+            System.out.print("═");
         }
+        System.out.print("║ Benvingut a tic tac toe! ");
+        System.out.print("╗");
     }
 }
