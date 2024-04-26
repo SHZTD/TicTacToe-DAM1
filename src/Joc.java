9b600572a431ba98088121b9bb978ad8fef40706
LuisHerrera4 <castanedaluis@elpuig.xeill.net>
2024-04-26 17:40:37 +0200

    Creación de la clase Joc, con sus metodos y atributos pertinentes

---
+++ src/Joc.java
@@ -0,0 +1,24 @@
+public class Joc {
+
+    private char[] taulell;
+
+    public char[] getTaulell() {
+        return taulell;
+    }
+
+    private short torn;
+
+    public short getTorn() {
+        return torn;
+    }
+
+    public void NovaPartida(){
+    }
+
+    public void jugar(short fila, String columna){
+    }
+
+    public boolean jugadaGuanyadora(short fila, String columna){
+        return false;
+    }
+}
