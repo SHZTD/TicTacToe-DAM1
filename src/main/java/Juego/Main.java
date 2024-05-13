9e4a29064618938c4136b36f3aade4c944394728
SHZTD <tdrag@elpuig.xeill.net>
2024-05-13 17:54:59 +0200

    Guardar partida

--- src/main/java/Juego/Main.java
+++ src/main/java/Juego/Main.java
@@ -1,10 +1,14 @@
 package Juego;
 
+import java.io.IOException;
+import java.text.ParseException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ParseException {
         TUI tui = new TUI();
         tui.mostrarMenu();
         Joc j = new Joc();
         j.novaPartida();
+        j.guardarPartida();
     }
 }
