f4b20ae992b3e1e5837b7512f4d606b299d7724c
LuisHerrera4 <castanedaluis@elpuig.xeill.net>
2024-05-14 17:47:02 +0200

    Inicio del volver atras despues de dar a la opción volver atras y tambien creacion del metodo configurarTamaño

--- src/main/java/Juego/TUI.java
+++ src/main/java/Juego/TUI.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class TUI {
     private Scanner sc = new Scanner(System.in);
 
-    public int choice;
     public int mostrarMenu() {
         System.out.print(
                 """
@@ -21,8 +20,8 @@ public class TUI {
                         ║ 4. Sortir                ║
                         ╚══════════════════════════╝
                         Selecciona l'opció que vols: """);
-        choice = sc.nextInt();
-        return choice;
+
+        return sc.nextInt();
     }
 
 
@@ -38,6 +37,18 @@ public class TUI {
                         ║ 2. Enrere                ║
                         ╚══════════════════════════╝
                         Selecciona l'opció que vols: """);
+        switch (sc.nextInt()){
+            case 1:
+                configurarTamany();
+                break;
+            case 2:
+                System.out.println("Tornem al menú principal");
+                break;
+        }
+    }
+
+    public int configurarTamany(){
+        return 1;
     }
 
     public void mostrarTaulell(char[][] taulell, short torn) {
